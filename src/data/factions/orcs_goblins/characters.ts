4bfe4d17da7524a805e1620acc7c08235cbeb54b
09421 <lasse@micorbis.dk>
2026-01-09 15:52:15 +0100

    more orc and goblin heroes

--- src/data/factions/orcs_goblins/characters.ts
+++ src/data/factions/orcs_goblins/characters.ts
@@ -1,6 +1,96 @@
 import { UnitDefinition } from '@/types/army';
+import { OG_STATS } from './stats';
 
 export const characters: UnitDefinition[] = [
+  {
+    id: 'blackorc_warlord',
+    name: 'Black Orc Warlord',
+    modelName: 'Black Orc Warlord',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 148,
+    stats: OG_STATS.blackOrcWarlord,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 3
+    },
+    specialRules: ['Quells animosity', 'Ignore panic induced by Goblins and other orc types'],
+    options: [
+      { id: 'war_boar', name: 'War Boar', points: 33, group: 'mount', optionProfile: { name: 'War Boar', stats: OG_STATS.warBoar } },
+      { id: 'wyvern', name: 'Wyvern', points: 167, group: 'mount', optionProfile: { name: 'Wyvern', stats: OG_STATS.wyvern } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+      { id: 'light_armor', name: 'Light armor', points: 0, group: 'armor' },
+      { id: 'heavy_armor', name: 'Heavy armor', points: 0, group: 'armor' },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'halberd', name: 'Halberd,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
+    ]
+  },
+  {
+    id: 'blackorc_hero',
+    name: 'Black Orc Hero',
+    modelName: 'Black Orc Hero',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 89,
+    stats: OG_STATS.blackOrcHero,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 3
+    },
+    specialRules: ['Quells animosity', 'Ignore panic induced by Goblins and other orc types'],
+    options: [
+      { id: 'war_boar', name: 'War Boar', points: 24, group: 'mount', optionProfile: { name: 'War Boar', stats: OG_STATS.warBoar } },
+      { id: 'wyvern', name: 'Wyvern', points: 158, group: 'mount', optionProfile: { name: 'Wyvern', stats: OG_STATS.wyvern } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+      { id: 'light_armor', name: 'Light armor', points: 0, group: 'armor' },
+      { id: 'heavy_armor', name: 'Heavy armor', points: 0, group: 'armor' },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'halberd', name: 'Halberd,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
+    ]
+  },
+  {
+    id: 'orc_warlord',
+    name: 'Orc Warlord',
+    modelName: 'Orc Warlord',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 100,
+    stats: OG_STATS.OrcWarlord,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 3
+    },
+    specialRules: ['Animosity', 'Ignore panic induced by Goblins'],
+    options: [
+      { id: 'war_boar', name: 'War Boar', points: 21, group: 'mount', optionProfile: { name: 'War Boar', stats: OG_STATS.warBoar } },
+      { id: 'boar_chariot', name: 'Boar Chariot', points: 52, group: 'mount', optionProfile: { name: 'War Boar', stats: OG_STATS.warBoar } },
+      { id: 'wyvern', name: 'Wyvern', points: 155, group: 'mount', optionProfile: { name: 'Wyvern', stats: OG_STATS.wyvern } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+      { id: 'light_armor', name: 'Light armor', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'halberd', name: 'Halberd,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
+      { id: 'bow', name: 'Bow', points: 10, group: 'ranged_weapon' },
+      { id: 'crossbow', name: 'Crossbow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
   {
     id: 'orc_hero',
     name: 'Orc Hero',
@@ -10,30 +100,243 @@ export const characters: UnitDefinition[] = [
     minSize: 1,
     maxSize: 1,
     pointsPerModel: 60,
-    stats: { M: 4, Ws: 5, Bs: 5, S: 4, T: 5, W: 2, I: 4, A: 3, Ld: 8 },
+    stats: OG_STATS.OrcHero,
     equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 2
+    },
     specialRules: ['Animosity', 'Ignore panic induced by Goblins'],
     options: [
-      { 
-        id: 'war_boar', 
-        name: 'War Boar', 
-        points: 16, 
-        group: 'mount',
-        optionProfile: 
-        {
-          name: 'War Boar',
-          stats: { M:7, Ws:4, Bs:0, S:3, T:4, W:1, I:3, A:1, Ld:3 } 
-        },
-      },
-      { id: 'boat_chariot', name: 'Boar Chariot', points: 52, group: 'mount' },
-      { id: 'wyvern', name: 'Wyvern', points: 150, group: 'mount' },
+      { id: 'war_boar', name: 'War Boar', points: 16, group: 'mount', optionProfile: { name: 'War Boar', stats: OG_STATS.warBoar } },
+      { id: 'boar_chariot', name: 'Boar Chariot', points: 52, group: 'mount', optionProfile: { name: 'War Boar', stats: OG_STATS.warBoar } },
+      { id: 'wyvern', name: 'Wyvern', points: 150, group: 'mount', optionProfile: { name: 'Wyvern', stats: OG_STATS.wyvern } },
+
       { id: 'shield', name: 'Shield', points: 0 },
       { id: 'light_armor', name: 'Light armor', points: 0 },
+
       { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
       { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
       { id: 'halberd', name: 'Halberd,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
       { id: 'bow', name: 'Bow', points: 10, group: 'ranged_weapon' },
       { id: 'crossbow', name: 'Crossbow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
+  {
+    id: 'savage_warlord',
+    name: 'Savage Orc Warlord',
+    modelName: 'Savage Orc Warlord',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 130,
+    stats: OG_STATS.OrcWarlord,
+    equipment: ['Hand Weapon', 'Magical Tattoos'],
+    magicAllowance: {
+      maxItems: 3
+    },
+    specialRules: ['Animosity', 'Frenzy', 'Magical Tattoos', 'Ignore panic induced by Goblins'],
+    options: [
+      { id: 'war_boar', name: 'War Boar', points: 27, group: 'mount', optionProfile: { name: 'War Boar', stats: OG_STATS.warBoar } },
+      { id: 'wyvern', name: 'Wyvern', points: 161, group: 'mount', optionProfile: { name: 'Wyvern', stats: OG_STATS.wyvern } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
+      { id: 'bow', name: 'Bow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
+  {
+    id: 'savage_hero',
+    name: 'Savage Orc Hero',
+    modelName: 'Savage Orc Hero',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 90,
+    stats: OG_STATS.OrcHero,
+    equipment: ['Hand Weapon', 'Magical Tattoos'],
+    magicAllowance: {
+      maxItems: 3
+    },
+    specialRules: ['Animosity', 'Frenzy', 'Magical Tattoos', 'Ignore panic induced by Goblins'],
+    options: [
+      { id: 'war_boar', name: 'War Boar', points: 20, group: 'mount', optionProfile: { name: 'War Boar', stats: OG_STATS.warBoar } },
+      { id: 'wyvern', name: 'Wyvern', points: 154, group: 'mount', optionProfile: { name: 'Wyvern', stats: OG_STATS.wyvern } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
+      { id: 'bow', name: 'Bow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
+  {
+    id: 'goblin_warlord',
+    name: 'Goblin Warlord',
+    modelName: 'Goblin Warlord',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 60,
+    stats: OG_STATS.goblinWarlord,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 3
+    },
+    specialRules: ['Animosity'],
+    options: [
+      { id: 'giant_wolf', name: 'Giant Wolf', points: 14, group: 'mount', optionProfile: { name: 'Giant Wolf', stats: OG_STATS.giantWolf } },
+      { id: 'wolf_chariot', name: 'Wolf Chariot', points: 44, group: 'mount', optionProfile: { name: 'Wolf Boar', stats: OG_STATS.giantWolf } },
+      { id: 'monstrou_spider', name: 'Monstrous Spider', points: 40, group: 'mount', optionProfile: { name: 'Monstrous Spider', stats: OG_STATS.monstrousSpider } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+      { id: 'light_armor', name: 'Light armor', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
+      { id: 'short_bow', name: 'Short Bow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
+  {
+    id: 'goblin_hero',
+    name: 'Goblin Hero',
+    modelName: 'Goblin Hero',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 36,
+    stats: OG_STATS.goblinHero,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 2
+    },
+    specialRules: ['Animosity'],
+    options: [
+      { id: 'giant_wolf', name: 'Giant Wolf', points: 11, group: 'mount', optionProfile: { name: 'Giant Wolf', stats: OG_STATS.giantWolf } },
+      { id: 'wolf_chariot', name: 'Wolf Chariot', points: 44, group: 'mount', optionProfile: { name: 'Wolf Boar', stats: OG_STATS.giantWolf } },
+      { id: 'monstrou_spider', name: 'Monstrous Spider', points: 37, group: 'mount', optionProfile: { name: 'Monstrous Spider', stats: OG_STATS.monstrousSpider } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+      { id: 'light_armor', name: 'Light armor', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'bow', name: 'Bow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
+  {
+    id: 'forest_goblin_warlord',
+    name: 'Forest Goblin Warlord',
+    modelName: 'Forest Goblin Warlord',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 60,
+    stats: OG_STATS.goblinWarlord,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 3
+    },
+    specialRules: ['Animosity'],
+    options: [
+      { id: 'giant_spider', name: 'Giant Spider', points: 13, group: 'mount', optionProfile: { name: 'Giant Spider', stats: OG_STATS.giantSpider } },
+      { id: 'monstrou_spider', name: 'Monstrous Spider', points: 40, group: 'mount', optionProfile: { name: 'Monstrous Spider', stats: OG_STATS.monstrousSpider } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
+      { id: 'short_bow', name: 'Short Bow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
+  {
+    id: 'forest_goblin_hero',
+    name: 'Forest Goblin Hero',
+    modelName: 'Forest Goblin Hero',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 36,
+    stats: OG_STATS.goblinHero,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 2
+    },
+    specialRules: ['Animosity'],
+    options: [
+      { id: 'giant_spider', name: 'Giant Spider', points: 10, group: 'mount', optionProfile: { name: 'Giant Spider', stats: OG_STATS.giantSpider } },
+      { id: 'monstrou_spider', name: 'Monstrous Spider', points: 37, group: 'mount', optionProfile: { name: 'Monstrous Spider', stats: OG_STATS.monstrousSpider } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+      { id: 'light_armor', name: 'Light armor', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'bow', name: 'Bow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
+  {
+    id: 'night_goblin_warlord',
+    name: 'Night Goblin Warlord',
+    modelName: 'Night Goblin Warlord',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 60,
+    stats: OG_STATS.goblinWarlord,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 3
+    },
+    specialRules: ['Animosity'],
+    options: [
+      { id: 'monstrou_spider', name: 'Monstrous Spider', points: 40, group: 'mount', optionProfile: { name: 'Monstrous Spider', stats: OG_STATS.monstrousSpider } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'great_weapon', name: 'Double handed weapon', points: 0, group: 'weapon' },
+      { id: 'short_bow', name: 'Short Bow', points: 10, group: 'ranged_weapon' },
+    ]
+  },
+  {
+    id: 'night_goblin_hero',
+    name: 'Night Goblin Hero',
+    modelName: 'Night Goblin Hero',
+    role: 'character',
+    type: 'infantry',
+    minSize: 1,
+    maxSize: 1,
+    pointsPerModel: 36,
+    stats: OG_STATS.goblinHero,
+    equipment: ['Hand Weapon'],
+    magicAllowance: {
+      maxItems: 2
+    },
+    specialRules: ['Animosity'],
+    options: [
+      { id: 'monstrou_spider', name: 'Monstrous Spider', points: 37, group: 'mount', optionProfile: { name: 'Monstrous Spider', stats: OG_STATS.monstrousSpider } },
+
+      { id: 'shield', name: 'Shield', points: 0 },
+      { id: 'light_armor', name: 'Light armor', points: 0 },
+
+      { id: 'add_hw', name: 'Additional hand weapon', points: 0, group: 'weapon'},
+      { id: 'spear', name: 'Spear,', points: 0, group: 'weapon' },
+      { id: 'bow', name: 'Bow', points: 10, group: 'ranged_weapon' },
     ]
   },
 ]
--- src/data/factions/orcs_goblins/stats.ts
+++ src/data/factions/orcs_goblins/stats.ts
@@ -4,40 +4,57 @@ export const OG_STATS: Record<string, StatBlock> = {
 
   //Infantry
   commonOrc:            { M: 4, Ws: 3, Bs: 3, S: 3, T: 4, W: 1, I: 2, A: 1, Ld: 7 },
-  commonOrcChampion:    { M: 4, Ws: 4, Bs: 4, S: 4, T: 4, W: 1, I: 3, A: 2, Ld: 7 },
-
   bigun:                { M: 4, Ws: 4, Bs: 3, S: 4, T: 4, W: 1, I: 3, A: 1, Ld: 7 },
-  bigunChampion:        { M: 4, Ws: 5, Bs: 4, S: 4, T: 4, W: 1, I: 4, A: 2, Ld: 7 },
-
   savageOrc:            { M: 4, Ws: 3, Bs: 3, S: 3, T: 4, W: 1, I: 2, A: 1, Ld: 7 },
-  savageChampion:       { M: 4, Ws: 4, Bs: 4, S: 4, T: 4, W: 1, I: 3, A: 2, Ld: 7 },
-
   blackOrc:             { M: 4, Ws: 4, Bs: 3, S: 4, T: 4, W: 1, I: 2, A: 1, Ld: 8 },
-  blackOrcChampion:     { M: 4, Ws: 5, Bs: 4, S: 5, T: 4, W: 1, I: 3, A: 2, Ld: 8 },
-  
   goblin:               { M: 4, Ws: 2, Bs: 3, S: 3, T: 3, W: 1, I: 2, A: 1, Ld: 5 },
-  goblinChampion:       { M: 4, Ws: 3, Bs: 4, S: 4, T: 3, W: 1, I: 3, A: 2, Ld: 5 },
-
   nightGoblin:          { M: 4, Ws: 2, Bs: 3, S: 3, T: 3, W: 1, I: 2, A: 1, Ld: 5 },  
-  nightGoblinChampion:  { M: 4, Ws: 3, Bs: 4, S: 4, T: 3, W: 1, I: 3, A: 2, Ld: 5 }, 
   nightGoblinFanatic:   { M: '2D6', Ws: 2, Bs: 3, S: 5, T: 3, W: 1, I: 1, A: '1D6', Ld: 5 },
-
   caveSquig:            { M: '2D6', Ws: 4, Bs: 0, S: 5, T: 3, W: 1, I: 5, A: 2, Ld: 2 },
-
-  forestGoblin:         { M: 4, Ws: 2, Bs: 3, S: 3, T: 3, W: 1, I: 2, A: 1, Ld: 5 },
-  forestGoblinChampion: { M: 4, Ws: 3, Bs: 4, S: 4, T: 3, W: 1, I: 3, A: 2, Ld: 5 },
-  
+  forestGoblin:         { M: 4, Ws: 2, Bs: 3, S: 3, T: 3, W: 1, I: 2, A: 1, Ld: 5 },  
   snotling:             { M: 4, Ws: 2, Bs: 2, S: 2, T: 2, W: 3, I: 3, A: 3, Ld: 4 },
-
   troll:                { M: 6, Ws: 3, Bs: 1, S: 5, T: 4, W: 3, I: 1, A: 3, Ld: 4 },
-
   ogre:                 { M: 6, Ws: 3, Bs: 2, S: 4, T: 5, W: 3, I: 3, A: 2, Ld: 7 },
-  ogreChampion:         { M: 6, Ws: 4, Bs: 3, S: 5, T: 5, W: 3, I: 4, A: 3, Ld: 7 },
   
+  //champions
+  commonOrcChampion:    { M: 4, Ws: 4, Bs: 4, S: 4, T: 4, W: 1, I: 3, A: 2, Ld: 7 },
+  bigunChampion:        { M: 4, Ws: 5, Bs: 4, S: 4, T: 4, W: 1, I: 4, A: 2, Ld: 7 },
+  savageChampion:       { M: 4, Ws: 4, Bs: 4, S: 4, T: 4, W: 1, I: 3, A: 2, Ld: 7 },
+  blackOrcChampion:     { M: 4, Ws: 5, Bs: 4, S: 5, T: 4, W: 1, I: 3, A: 2, Ld: 8 },
+  goblinChampion:       { M: 4, Ws: 3, Bs: 4, S: 4, T: 3, W: 1, I: 3, A: 2, Ld: 5 },
+  nightGoblinChampion:  { M: 4, Ws: 3, Bs: 4, S: 4, T: 3, W: 1, I: 3, A: 2, Ld: 5 }, 
+  forestGoblinChampion: { M: 4, Ws: 3, Bs: 4, S: 4, T: 3, W: 1, I: 3, A: 2, Ld: 5 },
+  ogreChampion:         { M: 6, Ws: 4, Bs: 3, S: 5, T: 5, W: 3, I: 4, A: 3, Ld: 7 },
+
+  //characters
+  blackOrcWarlord:      { M: 4, Ws: 7, Bs: 6, S: 5, T: 5, W: 3, I: 5, A: 4, Ld: 10 },
+  OrcWarlord:           { M: 4, Ws: 6, Bs: 6, S: 4, T: 5, W: 3, I: 5, A: 4, Ld: 9 },
+  goblinWarlord:        { M: 4, Ws: 5, Bs: 6, S: 4, T: 4, W: 3, I: 5, A: 4, Ld: 7 },
+
+  blackOrcHero:         { M: 4, Ws: 7, Bs: 6, S: 5, T: 5, W: 3, I: 5, A: 4, Ld: 9 },
+  OrcHero:              { M: 4, Ws: 5, Bs: 5, S: 4, T: 5, W: 2, I: 4, A: 3, Ld: 8 },
+  goblinHero:           { M: 4, Ws: 4, Bs: 5, S: 4, T: 4, W: 2, I: 4, A: 3, Ld: 6 },
+
+  blackOrcBsb:          { M: 4, Ws: 5, Bs: 4, S: 5, T: 4, W: 2, I: 3, A: 2, Ld: 8 },
+  orcBsb:               { M: 4, Ws: 4, Bs: 4, S: 4, T: 4, W: 2, I: 3, A: 2, Ld: 7 },
+  goblinBsb:            { M: 4, Ws: 3, Bs: 4, S: 4, T: 3, W: 2, I: 3, A: 2, Ld: 5 },
+
+  orcShamanLord:        { M: 4, Ws: 3, Bs: 3, S: 4, T: 5, W: 4, I: 5, A: 3, Ld: 8 },
+  orcMasterShaman:      { M: 4, Ws: 3, Bs: 3, S: 4, T: 5, W: 3, I: 4, A: 2, Ld: 7 },
+  orcShamanChampion:    { M: 4, Ws: 3, Bs: 3, S: 4, T: 5, W: 2, I: 3, A: 1, Ld: 7 },
+  orcShaman:            { M: 4, Ws: 3, Bs: 3, S: 3, T: 5, W: 1, I: 3, A: 1, Ld: 7 },
+
+  goblinShamanLord:     { M: 4, Ws: 2, Bs: 3, S: 4, T: 4, W: 4, I: 5, A: 3, Ld: 6 },
+  goblinShamanMaster:   { M: 4, Ws: 2, Bs: 3, S: 4, T: 4, W: 3, I: 4, A: 2, Ld: 5 },
+  goblinShamanChampion: { M: 4, Ws: 2, Bs: 3, S: 4, T: 4, W: 2, I: 3, A: 1, Ld: 5 },
+  goblinShaman:         { M: 4, Ws: 2, Bs: 3, S: 3, T: 4, W: 1, I: 3, A: 1, Ld: 5 },
+
+  //Mounts
   warBoar:              { M: 7, Ws: 4, Bs: 0, S: 3, T: 4, W: 1, I: 3, A: 1, Ld: 3 },
   giantWolf:            { M: 9, Ws: 4, Bs: 0, S: 3, T: 3, W: 1, I: 3, A: 1, Ld: 3 },
   giantSpider:          { M: 7, Ws: 3, Bs: 0, S: 3, T: 3, W: 1, I: 1, A: 1, Ld: 5 },
-
+  monstrousSpider:      { M: 7, Ws: 3, Bs: 0, S: 4, T: 4, W: 4, I: 1, A: 2, Ld: 5 },
+  wyvern:               { M: 6, Ws: 5, Bs: 0, S: 5, T: 6, W: 4, I: 4, A: 3, Ld: 5 },
 
   //monsters
   giant:                { M: 6, Ws: 3, Bs: 3, S: 7, T: 6, W: 6, I: 3, A: 'Special', Ld: 6 },
