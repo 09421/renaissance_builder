ec1ca211a34b0bdec7a5811e69361e5554a54a25
09421 <lasse@micorbis.dk>
2026-01-10 12:59:40 +0100

    empire knights

--- src/data/factions/empire/regiments.ts
+++ src/data/factions/empire/regiments.ts
@@ -249,4 +249,161 @@ export const regiments: UnitDefinition[] = [
       }
     ]
   },
+  {
+    id: 'white_wolf_knights',
+    name: 'Knights of the White Wolf',
+    modelName: 'Knight',
+    role: 'regiments',
+    type: 'Cavalry',
+    minSize: 5,
+    maxSize: null,
+    pointsPerModel: 20,
+    stats: EMP_STATS.knight,
+    additionalProfiles: [
+      {
+        name: 'War horse',
+        stats: EMP_STATS.Warhorse
+      }
+    ],
+    equipment: ['Hand Weapon', 'Double handed weapons', 'Full plate', 'Barding'],
+    specialRules: [],
+    options: [
+      {
+        id: 'captain',
+        name: 'Captain',
+        points: 30,
+        stats: EMP_STATS.captain,
+        isFixedCost: true
+      }
+    ]
+  },
+  {
+    id: 'Knights_Panther',
+    name: 'Knights Panther',
+    modelName: 'Knight',
+    role: 'regiments',
+    type: 'Cavalry',
+    minSize: 5,
+    maxSize: null,
+    pointsPerModel: 25,
+    stats: EMP_STATS.knight,
+    additionalProfiles: [
+      {
+        name: 'War horse',
+        stats: EMP_STATS.Warhorse
+      }
+    ],
+    equipment: ['Hand Weapon', 'Lances', 'Full plate', 'shield', 'Barding'],
+    specialRules: [],
+    options: [
+      {
+        id: 'captain',
+        name: 'Captain',
+        points: 30,
+        stats: EMP_STATS.captain,
+        isFixedCost: true
+      }
+    ]
+  },
+  {
+    id: 'blazing_sun',
+    name: 'Knights of the Blazing Sun',
+    modelName: 'Knight',
+    role: 'regiments',
+    type: 'Cavalry',
+    minSize: 5,
+    maxSize: null,
+    pointsPerModel: 25,
+    stats: EMP_STATS.knight,
+    additionalProfiles: [
+      {
+        name: 'War horse',
+        stats: EMP_STATS.Warhorse
+      }
+    ],
+    equipment: ['Hand Weapon', 'Lances', 'Full plate', 'shield', 'Barding'],
+    specialRules: [],
+    options: [
+      {
+        id: 'captain',
+        name: 'Captain',
+        points: 30,
+        stats: EMP_STATS.captain,
+        isFixedCost: true
+      }
+    ]
+  },
+  {
+    id: 'teutogen_Foot',
+    name: 'Teutogen Foot Knights',
+    modelName: 'Knight',
+    role: 'regiments',
+    type: 'Infantry',
+    minSize: 5,
+    maxSize: null,
+    pointsPerModel: 13,
+    stats: EMP_STATS.knight,
+    equipment: ['Hand Weapon', 'Double handed weapons', 'Full plate'],
+    specialRules: [],
+    options: [
+      {
+        id: 'captain',
+        name: 'Captain',
+        points: 30,
+        stats: EMP_STATS.captain,
+        isFixedCost: true
+      }
+    ]
+  },
+  {
+    id: 'reiksguard_foot',
+    name: 'Reiksguard Foot Knights',
+    modelName: 'Knight',
+    role: 'regiments',
+    type: 'Infantry',
+    minSize: 5,
+    maxSize: null,
+    pointsPerModel: 11,
+    stats: EMP_STATS.knight,
+    equipment: ['Hand Weapon', 'Full plate', 'shield'],
+    specialRules: [],
+    options: [
+      { id: 'great_weapon', name: 'Double handed weapon', points: 2, replaces:'shield' },
+      {
+        id: 'commander',
+        name: 'Commander',
+        points: 50,
+        stats: EMP_STATS.commander,
+        isFixedCost: true
+      }
+    ]
+  },
+  {
+    id: 'reiksguard',
+    name: 'Reiksguard Knights',
+    modelName: 'Knight',
+    role: 'regiments',
+    type: 'Cavalry',
+    minSize: 5,
+    maxSize: null,
+    pointsPerModel: 25,
+    stats: EMP_STATS.knight,
+    additionalProfiles: [
+      {
+        name: 'War horse',
+        stats: EMP_STATS.Warhorse
+      }
+    ],
+    equipment: ['Hand Weapon', 'Lances', 'Full plate', 'shield', 'Barding'],
+    specialRules: [],
+    options: [
+      {
+        id: 'commander',
+        name: 'Commander',
+        points: 50,
+        stats: EMP_STATS.commander,
+        isFixedCost: true
+      }
+    ]
+  },
 ]
